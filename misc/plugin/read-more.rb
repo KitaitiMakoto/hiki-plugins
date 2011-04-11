6d1514d94d6e53d9be2042975375f83aa05b349c
KITAITI Makoto <KitaitiMakoto@gmail.com>
2011-04-12 00:29:08 +0900

    read-more.rb: Add toggling class to trigger anchor

--- misc/plugin/read-more.rb
+++ misc/plugin/read-more.rb
@@ -12,32 +12,42 @@
 # <<<
 # {{read_more(MoreText)}}
 # >>>
+#
+# ! Styling
+# Anchor element with hidden content has class 'read-more-show',
+# and it will be removed when the content will be shown by clicking the anchor.
+# So you can style the anchor with toggling color, image and so on.
+#
+# ! To do
+# * User specified text of anchor
+# * User specified sliding speed 
 @read_more_count = 0
 
 def read_more(page)
   @read_more_count += 1
   <<EOQ
-  <div>
-    <div class="read-more" style="display: none;">
+  <div class="read-more">
+    <div style="display: none;">
 #{quote_page(page)}
     </div>
-    <a href="?#{page}" class="read-more">#{read_more_show_label}</a>
+    <a href="./?#{page}" class="read-more-show">#{read_more_show_label}</a>
   </div>
 EOQ
 end
 
 add_footer_proc do
-  <<EOS
+  @read_more_count == 0 ? '' : <<EOS
 <script type="text/javascript" charset="#{@conf.charset}">
   jQuery.fn.readMoreToggleText = function (first, second) {
     return this.text(this.text() == first ? second : first);
   };
   (function ($) {
-    $('a.read-more').click(function (event) {
+    $('div.read-more a').click(function (event) {
       event.preventDefault();
       var anchor = $(event.currentTarget);
-      anchor.prev('.read-more').slideToggle('normal', function () {
-        anchor.readMoreToggleText('#{read_more_show_label}', '#{read_more_hide_label}')
+      anchor.prev('div').slideToggle('normal', function () {
+        anchor.toggleClass('read-more-show').
+                 readMoreToggleText('#{read_more_show_label}', '#{read_more_hide_label}');
       });
     });
   })(jQuery);
