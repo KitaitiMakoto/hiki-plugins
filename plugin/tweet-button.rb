77671386ded7031cb1d238f7ba88e7bee64dd985
KITAITI Makoto <KitaitiMakoto@gmail.com>
2018-04-13 16:09:06 +0900

    Add tweet_button plugin

---
+++ plugin/tweet-button.rb
@@ -0,0 +1,5 @@
+def tweet_button
+  <<EOS
+<a href="https://twitter.com/share?hashtags=%E3%82%AF%E3%83%AA%E3%82%B9%E3%82%BF%E3%83%8B%E3%82%A2" class="twitter-share-button" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+EOS
+end
